22d059b7ecfb3e29a5cfbbf31429087ca4062b6a
Haroldini <lemonboi42@gmail.com>
2024-05-07 11:41:11 +0100

    added requirements to setup

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from setuptools import setup, find_packages
 
+# Read the contents of the readme file for the setup desc
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
+# Read the contents of the requirements file for setup reqs
+with open("requirements.txt", "r") as f:
+    requirements = f.read().splitlines()
+
 setup(
-    name='sidt',
-    license='MIT',
+    name="sidt",
+    license="MIT",
     packages=find_packages(),
-    install_requires=['tls_client', 'tqdm', 'beautifulsoup4'],
+    install_requires=requirements,
 )
