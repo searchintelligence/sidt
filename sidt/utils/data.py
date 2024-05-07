f2b20629cc5facffac76f5a4f3107ddada85fb19
Haroldini <haroldini42@gmail.com>
2024-05-07 17:53:17 +0100

    open_dir(), excel_column_converter()

--- sidt/utils/data.py
+++ sidt/utils/data.py
@@ -50,3 +50,39 @@ def flatten_structure(structure):
             result.append(item)
     return result
 
+
+def excel_column_converter(value):
+    """
+    Converts between a bijective base-26 number and its corresponding alphabetic string and vice versa.
+
+    Args:
+        value (int or str): Numeric index or alphabetic string to be converted.
+
+    Example:
+    - excel_column_converter(1) -> 'A'
+    - excel_column_converter('A') -> 1
+    - excel_column_converter(28) -> 'AB'
+    - excel_column_converter('AB') -> 28
+    """
+
+    # Convert integer to string
+    if isinstance(value, int):
+        if value < 1:
+            raise ValueError("Index must be at least 1.")
+        name = []
+        while value > 0:
+            value, remainder = divmod(value - 1, 26)
+            name.append(chr(65 + remainder))
+        return ''.join(reversed(name))
+    
+    # Convert string to integer
+    elif isinstance(value, str):
+        number = 0
+        for char in value.upper():
+            if not char.isalpha() or ord(char) < ord('A') or ord(char) > ord('Z'):
+                raise ValueError("String must consist only of uppercase letters from A to Z.")
+            number = number * 26 + (ord(char) - ord('A') + 1)
+        return number
+    
+    else:
+        raise TypeError("Input must be an integer or a string.")
--- sidt/utils/io.py
+++ sidt/utils/io.py
@@ -1,10 +1,10 @@
 import csv
-import inspect
 import os
-
+import sys
+import inspect
+import subprocess
 from dataclasses import asdict, fields
 from enum import Enum
-from tqdm import tqdm
 
 import pandas as pd
 
@@ -81,7 +81,10 @@ def dump(data, filename):
 
 
 def get_current_path():
-    """Returns the path of the python file from which this function is called."""
+    """
+    Returns the absolute directory path of the python file from which this function is called.
+    """
+
     caller_frame = inspect.stack()[1]
     caller_path = caller_frame.filename
     caller_directory = os.path.dirname(os.path.abspath(caller_path))
@@ -114,3 +117,26 @@ def dfs_to_xlsx(dfs, file_path):
     with pd.ExcelWriter(file_path, engine="xlsxwriter") as writer:
         for sheet_name, df in dfs.items():
             df.to_excel(writer, sheet_name=sheet_name)
+
+
+def open_dir(target_dir, return_pid=False):
+    """
+    Opens a directory or file in the system's default file explorer or associated application.
+    Supports Windows, macOS, and Linux operating systems.
+
+    Args:
+        target_dir (str): The directory path to open.
+        return_pid (bool): If True, returns the process ID (int) of the file explorer.
+    """
+
+    process = None
+    if sys.platform == "win32":
+        process = subprocess.Popen(['explorer', target_dir])
+    elif sys.platform == "darwin":
+        process = subprocess.Popen(['open', target_dir])
+    else:
+        process = subprocess.Popen(['xdg-open', target_dir])
+    
+    if return_pid:
+        return process.pid
+
