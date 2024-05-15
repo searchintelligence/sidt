c0c0fcbf5ece8c0bef8b33840a6cc0541e86b65d
Arek Zajac <arekzajac@outlook.com>
2024-05-15 11:21:13 +0100

    Unit testing for Trustpilot interface

--- sidt/interfaces/trustpilot.py
+++ sidt/interfaces/trustpilot.py
@@ -1,42 +1,25 @@
 from ..utils.api import make_request
-from dataclasses import dataclass
 from bs4 import BeautifulSoup
 from tqdm import tqdm
 
 
-@dataclass
-class Review:
-    heading: str
-    body: str
-    rating: int
-    keywords: list[str]
-
-
-@dataclass
-class Trustpilot:
-    id: str
-    name: str
-    review_count: int
-    rating_class: str
-    score: float
-    category: str
-
-
-def process_reviews(response) -> list[Review]:
+def process_reviews(response):
     reviews = []
     for review in response["pageProps"]["reviews"]:
-        heading = review["title"]
-        body = review["text"]
-        rating = review["rating"]
-        reviews.append(Review(heading, body, rating))
+        reviews.append({
+            "title": review["title"],
+            "body": review["text"],
+            "rating": review["rating"]
+        })
     return reviews
 
 
-def get_reviews(id, headers) -> list[Review]:
+def get_reviews(id):
     reviews = []
-    url = f"https://www.trustpilot.com/_next/data/businessunitprofile-consumersite-2.388.0/review/{id}.json?businessUnit={id}"
+    headers = {'x-nextjs-data': '1'}
+    url = f"https://www.trustpilot.com/_next/data/businessunitprofile-consumersite-2.427.0/review/{id}.json?businessUnit={id}"
     r = make_request(url=url, method="GET", headers=headers).json()
-    reviews.append(process_reviews(r))
+    reviews.extend(process_reviews(r))
 
     pages = r["pageProps"]["filters"]["pagination"]["totalPages"]
     if pages > 1:
@@ -44,32 +27,65 @@ def get_reviews(id, headers) -> list[Review]:
             url += f"&?page={p}"
             r = make_request(url=url, method="GET", headers=headers).json()
             reviews.append(process_reviews(r))
-    
+
     return reviews
 
 
-def get_site_info(id, headers) -> Trustpilot:
+def get_site_info(id: str):
     url = f"https://www.trustpilot.com/review/{id}"
-    r = make_request(url=url, method="GET", headers=headers).text
+    r = make_request(url=url, method="GET").text
 
-    soup = BeautifulSoup(r, 'html.parser').find(attrs={"id": "business-unit-title"})
+    soup = BeautifulSoup(r, 'html.parser').find(
+        attrs={"id": "business-unit-title"})
 
     name = soup.find("h1").find_all("span")[0].get_text(strip=True)
-    review_count = int(soup.find("span", role="link").get_text().split("•")[0].strip().replace(",", ""))
-    rating_class = soup.find("span", role="link").get_text().split("•")[-1].strip()
-    score = soup.find("div", {"data-rating-component": "true"}).find("p").get_text(strip=True)
+    review_count = int(soup.find("span", role="link").get_text().split("•")[
+                       0].strip().replace(",", ""))
+    rating_class = soup.find(
+        "span", role="link").get_text().split("•")[-1].strip()
+    score = soup.find(
+        "div", {"data-rating-component": "true"}).find("p").get_text(strip=True)
 
     category_link = soup.find("a", href=lambda x: x and "/categories/" in x)
     category = category_link.get_text(strip=True) if category_link else None
 
-    return Trustpilot(id, name, review_count, rating_class, score, category)
+    return {
+        "id": id,
+        "name": name,
+        "review_count": review_count,
+        "rating_class": rating_class,
+        "score": score,
+        "category": category
+    }
 
 
-def make_search(query:str, result_size:int=100, country:str="US"):
+def make_search(query: str, result_size: int = 100, country: str = "US"):
+    """
+    Performs a search on Trustpilot for business units based on the given query.
+
+    Args:
+        query (str): The search query.
+        result_size (int, optional): The maximum number of results to return. Defaults to 100.
+        country (str, optional): The country to search in. Defaults to "US".
+
+    Raises:
+        ValueError: If result_size is greater than 100.
+
+    Returns:
+        list: A list of dictionaries containing information about the business units found in the search.
+            Each dictionary contains the following keys:
+            - id: The identifying name of the business unit.
+            - name: The display name of the business unit.
+            - numberOfReviews: The number of reviews for the business unit.
+            - trustScore: The trust score of the business unit.
+            - stars: The star rating of the business unit.
+            - websiteUrl: The website URL of the business unit.
+            - verified: Whether the business unit is verified or not.
+    """
 
     if result_size > 100:
         raise ValueError("result_size must be less than or equal to 100")
-    
+
     results = []
 
     url = f"https://www.trustpilot.com/api/consumersitesearch-api/businessunits/search?country={country}&pageSize={result_size}&query={query}"
@@ -85,5 +101,5 @@ def make_search(query:str, result_size:int=100, country:str="US"):
             "websiteUrl": item["websiteUrl"],
             "verified": item["verified"],
         })
-    
+
     return results
--- tests/test_trustpilot.py
+++ tests/test_trustpilot.py
@@ -5,32 +5,11 @@ from sidt.interfaces.trustpilot import *
 
 class TestTrustpilot(unittest.TestCase):
 
-    def setUp(self):
-        self.headers = {
-            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-            'accept-language': 'en-US,en;q=0.9',
-            'cache-control': 'max-age=0',
-            # 'cookie': 'TP.uuid=3752ac4e-7a0c-45ef-9805-845abfc397d5; OptanonAlertBoxClosed=2024-03-30T19:49:59.728Z; OptanonConsent=isGpcEnabled=0&datestamp=Fri+May+03+2024+20%3A04%3A15+GMT%2B0100+(British+Summer+Time)&version=6.28.0&isIABGlobal=false&hosts=&consentId=83967eae-b7be-46e3-b505-2dbf8058d0ca&interactionCount=2&landingPath=NotLandingPage&groups=C0001%3A1%2CC0002%3A0%2CC0003%3A0%2CC0004%3A0&geolocation=GB%3BENG&AwaitingReconsent=false',
-            'dnt': '1',
-            'priority': 'u=0, i',
-            'referer': 'https://www.trustpilot.com/',
-            'sec-ch-ua': '"Not-A.Brand";v="99", "Chromium";v="124"',
-            'sec-ch-ua-mobile': '?0',
-            'sec-ch-ua-platform': '"macOS"',
-            'sec-fetch-dest': 'document',
-            'sec-fetch-mode': 'navigate',
-            'sec-fetch-site': 'same-origin',
-            'sec-fetch-user': '?1',
-            'upgrade-insecure-requests': '1',
-            'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36',
-        }
+    def test_get_site_info(self):
+        site_info = get_site_info("trustpilot.com")
 
-    def test_getSiteInfo(self):
-        site_info = get_site_info("trustpilot.com", self.headers)
-
-        self.assertIsInstance(site_info, Trustpilot)
-        self.assertEqual(site_info.name, "Trustpilot")
-        self.assertEqual(site_info.category, "Review Site")
+        self.assertEqual(site_info["name"], "Trustpilot")
+        self.assertEqual(site_info["category"], "Review Site")
 
     def test_make_search(self):
         results = make_search("trustpilot", 10, "US")
@@ -45,6 +24,19 @@ class TestTrustpilot(unittest.TestCase):
 
         self.assertIsInstance(results[0]["stars"], int)
         self.assertIsInstance(results[0]["trustScore"], float)
+
+    def test_get_reviews(self):
+        reviews = get_reviews("yeezy.com")
+        self.assertIsInstance(reviews, list)
+
+        self.assertIsInstance(reviews[0], dict)
+        self.assertIn("title", reviews[0])
+        self.assertIn("body", reviews[0])
+        self.assertIn("rating", reviews[0])
+
+        self.assertIsInstance(reviews[0]["title"], str)
+        self.assertIsInstance(reviews[0]["body"], str)
+        self.assertIsInstance(reviews[0]["rating"], int)
 
 
 if __name__ == '__main__':
