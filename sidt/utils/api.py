7b8bc2d3ed13f1b8b1343bf319043af999fa44b1
Arek Zajac <arekzajac@outlook.com>
2024-05-21 08:33:14 +0100

    Update api.py

--- sidt/utils/api.py
+++ sidt/utils/api.py
@@ -11,11 +11,11 @@ def new_session():
     )
 
 
-def make_request(url:str, method:str="GET", persistant:bool=True, headers:dict=None, cookies:dict=None, params:dict=None):
+def make_request(url:str, method:str="GET", persistant:bool=True, headers:dict=None, cookies:dict=None, params:dict=None, json:dict=None):
     session = new_session()
     while True:
         response = session.execute_request(url=url, method=method, headers=headers,
-                               cookies=cookies, params=params)
+                               cookies=cookies, params=params, json=json, allow_redirects=True)
         if response.status_code != 200 and persistant:
             tqdm.write(f"Error {response.status_code} requesting {url}")
             time.sleep(5)
