085f9653df3b43baebfb637e1d2f849826ac3735
Haroldini <haroldini42@gmail.com>
2024-06-11 13:42:41 +0100

    invalid long_description2

--- setup.py
+++ setup.py
@@ -1,9 +1,5 @@
 from setuptools import setup, find_packages
 
-# Read the contents of the readme file for the setup desc
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
 # Read the contents of the requirements file for setup reqs
 with open("requirements.txt", "r", encoding="utf-8") as f:
     requirements = f.read().splitlines()
