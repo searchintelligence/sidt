361f684138e61a53932bd0f814fe81c1bb6b10da
Haroldini <haroldini42@gmail.com>
2024-06-12 16:52:33 +0100

    colors class

---
+++ sidt/utils/colors.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+
+class Theme(Enum):
+    BLACK = "#141414"
+    WHITE = "#E3E3E3"
+    PURPLE = "#886AB4"
+    TEAL = "#3892A6"
+    GREEN = "#23A974"
+    RED = "#D9483E"
+    BLUE = "#4452A3"
