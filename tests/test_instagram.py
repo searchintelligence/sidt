58dd27e9c6cba70e429ddb73717a788c6440e3bd
Arek Zajac <arekzajac@outlook.com>
2024-06-24 14:03:25 +0100

    Instagram scraping re-do

--- tests/test_instagram.py
+++ tests/test_instagram.py
@@ -5,30 +5,47 @@ from sidt.interfaces.instagram import *
 
 class TestInstagram(unittest.TestCase):
 
-    def test_get_hashtag_popularity(self):
-        pop = get_hashtag_popularity("python")
+    def test_get_profile_id(self):
+        id = get_profile_id("brandoncoleman")
+        self.assertIsInstance(id, int)
+        self.assertGreater(id, 0)
+        print(id)
 
-        self.assertIsInstance(pop, int)
-        self.assertGreater(pop, 0)
-        print(pop)
+    def test_generate_session_id(self):
+        s = generate_session_id()
+        self.assertIsInstance(s, str)
+        self.assertGreater(len(s), 0)
+        print(s)
 
-    def test_get_profile_info(self):
-        info = get_profile_info("nasa")
-        self.assertIn("follower_count", info)
-        self.assertIn("following_count", info)
-        self.assertIsInstance(info["follower_count"], int)
-        self.assertIsInstance(info["following_count"], int)
-        self.assertGreater(info["follower_count"], 0)
-        self.assertGreater(info["following_count"], 0)
-        print(info)
+    def test_make_search_query(self):
+        q = make_search_query("nasa")
+        print(q)
+
+    def test_user_info(self):
+        username = "brandoncoleman"
+        i = get_user_info(get_profile_id(username))
+        self.assertIsInstance(i, dict)
+        self.assertGreater(len(i), 0)
+        self.assertDictContainsSubset({"username": username}, i)
+        self.assertDictContainsSubset({"id": 12032649}, i)
+        self.assertIsInstance(i["followers"], int)
+        self.assertIsInstance(i["following"], int)
+        self.assertIsInstance(i["posts"], int)
+        self.assertGreater(i["followers"], 0)
+        self.assertGreater(i["following"], 0)
+        self.assertGreater(i["posts"], 0)
+        self.assertIsInstance(i["biography"], str)
+        print(i)
     
-    def test_get_inzpire_data(self):
-        data = get_inzpire_data("drake.maye", currency="USD", posts=1, stories=1)
-        print(data)
+    def test_user_feed(self):
+        f = get_user_feed(get_profile_id("rossbutler"))
+        self.assertIsInstance(f, list)
+        self.assertGreater(len(f), 0)
+        print(len(f))
     
-    def test_get_picuki_data(self):
-        data = get_picuki_data("dariusmuasau")
-        print(data)
+    def test_generate_user_analysis(self):
+        a = generate_user_analysis("rossbutler")
+        print(a)
 
 
 if __name__ == '__main__':
