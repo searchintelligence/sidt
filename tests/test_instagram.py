f8bda8f47f2f11d239428323c59da2d010c3ee46
Arek Zajac <arekzajac@outlook.com>
2024-07-08 15:41:01 +0100

    Full refactor of instagram scraper

--- tests/test_instagram.py
+++ tests/test_instagram.py
@@ -5,31 +5,29 @@ from sidt.interfaces.instagram import *
 
 class TestInstagram(unittest.TestCase):
 
+    def __init__(self, *args, **kwargs):
+        super(TestInstagram, self).__init__(*args, **kwargs)
+        self.insta = Instagram("64879496178%3AEbd7XfUecybXa3%3A17%3AAYfaTaDENGLHA5EsmUv0kMU_MY399qG-lCuW5AY6bQ")
+
     def test_get_profile_id(self):
-        id = get_profile_id("brandoncoleman")
+        id = self.insta.get_profile_id("brandoncoleman")
         self.assertIsInstance(id, int)
         self.assertGreater(id, 0)
         print(id)
-
-    def test_generate_session_id(self):
-        s = generate_session_id()
-        self.assertIsInstance(s, str)
-        self.assertGreater(len(s), 0)
-        print(s)
     
     def test_get_hashtag_populatiry(self):
-        pop = get_hashtag_popularity("fyp")
+        pop = self.insta.get_hashtag_popularity("fyp")
         self.assertIsInstance(pop, int)
         self.assertGreater(pop, 0)
         print(pop)
 
     def test_make_search_query(self):
-        q = make_search_query("nasa")
+        q = self.insta.make_search_query("nasa")
         print(q)
 
     def test_user_info(self):
         username = "brandoncoleman"
-        i = get_user_info(get_profile_id(username))
+        i = self.insta.get_user_info(self.insta.get_profile_id(username))
         self.assertIsInstance(i, dict)
         self.assertGreater(len(i), 0)
         self.assertDictContainsSubset({"username": username}, i)
@@ -44,13 +42,13 @@ class TestInstagram(unittest.TestCase):
         print(i)
     
     def test_user_feed(self):
-        f = get_user_feed(get_profile_id("rossbutler"))
+        f = self.insta.get_user_feed(self.insta.get_profile_id("rossbutler"))
         self.assertIsInstance(f, list)
         self.assertGreater(len(f), 0)
         print(len(f))
     
     def test_generate_user_analysis(self):
-        a = generate_user_analysis("rossbutler")
+        a = self.insta.generate_user_analysis("rossbutler")
         print(a)
 
 
